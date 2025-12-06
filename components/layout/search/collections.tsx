4221db48619d0ec61a4a1635a23e8c009e25d7e8
fireproai <oliver@midlandfire.direct>
2025-12-06 15:13:22 +0000

    Update collections.tsx
    1513

--- components/layout/search/collections.tsx
+++ components/layout/search/collections.tsx
@@ -1,22 +1,16 @@
 import clsx from 'clsx';
 import { Suspense } from 'react';
-
-import { getCollections } from 'lib/shopify';
 import FilterList from './filter';
-
-const skeleton = 'mb-3 h-4 w-5/6 animate-pulse rounded-sm';
-const activeAndTitles = 'bg-neutral-800 dark:bg-neutral-300';
-const items = 'bg-neutral-400 dark:bg-neutral-700';
 
 function CollectionList({ collections }: { collections: any[] }) {
   return <FilterList list={collections} title="Collections" />;
 }
 
-// 🚀 This is now a SERVER COMPONENT
-export default async function Collections() {
-  // fetch on server
-  const collections = await getCollections();
+const skeleton = 'mb-3 h-4 w-5/6 animate-pulse rounded-sm';
+const activeAndTitles = 'bg-neutral-800 dark:bg-neutral-300';
+const items = 'bg-neutral-400 dark:bg-neutral-700';
 
+export default function Collections({ collections }: { collections: any[] }) {
   return (
     <Suspense
       fallback={
