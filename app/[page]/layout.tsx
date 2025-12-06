189c75bb0b4c2d9007d30e1754d75db70a1f72d2
fireproai <oliver@midlandfire.direct>
2025-12-06 13:11:57 +0000

    Update layout.tsx
    layout

--- app/[page]/layout.tsx
+++ app/[page]/layout.tsx
@@ -1,12 +1,18 @@
 import Footer from 'components/layout/footer';
+import { getMenu } from 'lib/shopify';
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default async function Layout({ children }: { children: React.ReactNode }) {
+  // Fetch footer menu on the server
+  const menu = await getMenu('next-js-frontend-footer-menu');
+
   return (
     <>
       <div className="w-full">
         <div className="mx-8 max-w-2xl py-20 sm:mx-auto">{children}</div>
       </div>
-      <Footer />
+
+      {/* Pass menu to synchronous Footer component */}
+      <Footer menu={menu} />
     </>
   );
 }
