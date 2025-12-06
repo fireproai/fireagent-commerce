9876fdb869de2cdca32c6e540200342bd8b1ffe2
fireproai <oliver@midlandfire.direct>
2025-12-06 17:39:05 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,7 @@ import { ThreeItemGrid } from 'components/grid/three-items';
 import Footer from 'components/layout/footer';
 import { getMenu } from 'lib/shopify';
 
-export const metadata = {
-  description:
-    'High-performance ecommerce store built with Next.js, Vercel, and Shopify.',
-  openGraph: {
-    type: 'website'
-  }
-};
-
 export default async function HomePage() {
-  // Fetch footer menu
   const footerMenu = await getMenu('next-js-frontend-footer-menu');
 
   return (
