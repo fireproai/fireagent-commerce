697ea4786b146caea339413ab2ee0fc775f92eab
fireproai <oliver@midlandfire.direct>
2025-12-06 15:51:57 +0000

    Update opengraph-image.tsx
    1551

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -1,5 +1,35 @@
-import OpengraphImage from 'components/opengraph-image';
+import { ImageResponse } from 'next/og';
 
-export default async function Image() {
-  return await OpengraphImage();
+export const size = {
+  width: 1200,
+  height: 630
+};
+
+export const contentType = 'image/png';
+
+export default function Image() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 56,
+          color: 'white',
+          background: '#000',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          textAlign: 'center',
+          padding: '40px'
+        }}
+      >
+        FireAgent
+      </div>
+    ),
+    {
+      width: size.width,
+      height: size.height
+    }
+  );
 }
