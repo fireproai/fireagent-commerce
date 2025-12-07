4aa86065e495ec4f8b96c9ec2bdf7b8112e6f9bc
fireproai <oliver@midlandfire.direct>
2025-12-07 09:29:35 +0000

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    dynamicIO: true,
+    generateOGImageRoutes: false, // Disable OG routes to prevent “use cache”
   },
 };
 
