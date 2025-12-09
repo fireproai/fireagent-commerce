ac62ebc5fe2708c504da8a303d88227d7ff14919
fireproai <oliver@midlandfire.direct>
2025-12-09 13:22:43 +0000

    Mark dynamic layout as force-dynamic

--- app/(dynamic)/layout.tsx
+++ app/(dynamic)/layout.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { CartProvider } from 'components/cart/cart-context';
 import Navbar from 'components/layout/navbar/server-navbar';
 import { WelcomeToast } from 'components/welcome-toast';
